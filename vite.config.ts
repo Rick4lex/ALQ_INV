674bafadf2a28f10a4f78d6e54c62b52bc7e037f
Ricardo Álvarez M. <140867844+Rick4lex@users.noreply.github.com>
2025-11-02 00:02:33 -0500

    chore: Update dependencies and project name
    Updates project dependencies to newer versions and refactors the project name in `package.json` and `package-lock.json` to "alquima-mizu-gestor".
    
    Removes unused Jekyll GitHub Pages workflow configuration.
    
    Adjusts `index.html` to reference local scripts correctly and updates Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ALQ_INV/',
   plugins: [react()],
+  base: '/ALQ_INV/',
 })
